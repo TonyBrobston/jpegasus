c187d0decb59d71983ef15e02cbc5b1092a48b35
Tony Brobston <tbrobston@hy-vee.com>
2018-12-20 14:56:08 -0600

    Move to module.exports

--- src/compressor.js
+++ src/compressor.js
@@ -1,14 +1,10 @@
 import scaleService from './services/sizing/scaleService';
 import qualityService from './services/sizing/qualityService';
 
-const compress = async (file) => {
+module.exports.compress = async (file) => {
     const scale = 0.29;
     const quality = 0.5;
 
     const canvas = await scaleService.toCanvas(file, scale);
     return qualityService.toFile(canvas, quality, file.name);
-};
-
-export default {
-    compress
 };
