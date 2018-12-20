3546110cc594b4f44eff4625b27d372b0052f813
Tony Brobston <tbrobston@hy-vee.com>
2018-12-20 15:20:47 -0600

    Moving to export default

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import scaleService from './services/sizing/scaleService';
 import qualityService from './services/sizing/qualityService';
 
-module.exports.compress = async (file) => {
+const compress = async (file) => {
     const scale = 0.29;
     const quality = 0.5;
 
     const canvas = await scaleService.toCanvas(file, scale);
     return qualityService.toFile(canvas, quality, file.name);
+};
+
+export default {
+    compress
 };
