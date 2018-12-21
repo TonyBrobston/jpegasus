019c895095e0e812b3f3a49a6145c181e8b7cb12
Tony Brobston <anthonybrobston@gmail.com>
2018-12-20 22:29:11 -0600

    Adding defaults of 1200 max width/height and compress photo by .5

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import scaleService from './services/sizing/scaleService';
 import qualityService from './services/sizing/qualityService';
 
-const compress = async (file, options = {}) => {
+const compress = async (file, options = {
+    maxHeight: 1200,
+    maxWidth: 1200,
+    targetFileSize: 500000
+}) => {
     const canvas = await scaleService.toCanvas(file, options);
     return qualityService.toFile(file, canvas, options);
 };
