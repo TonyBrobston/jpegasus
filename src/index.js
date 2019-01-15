ceda9945e870d32215a0556ef94da02fb56257ff
Tony Brobston <tbrobston@hy-vee.com>
2019-01-15 15:20:01 -0600

    ie11 doesn't support includes

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ module.exports.compress = async (file, options = {
     maxWidth: 16250,
     quality: 0.5
 }) => {
-    const isValidFile = typeof file === 'object' && file !== null && file.size > 0 && file.type.includes('image/');
+    const isValidFile = typeof file === 'object' && file !== null && file.size > 0 && file.type.indexOf('image/') >= 0;
 
     if (isValidFile) {
         const exifOrientation = await fileService.getOrientation(file);
