3de5ebdd3ab8b8763f404dbddc048739775eb1e4
Tony Brobston <tbrobston@hy-vee.com>
2019-02-14 14:54:50 -0600

    Increase amount of characters allowed on one line

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,12 @@
 module.exports = {
-    extends: "google",
-    parser: "babel-eslint",
+    extends: 'google',
+    parser: 'babel-eslint',
     rules: {
+        'max-len': [
+            'error', {
+                'code': 120
+            }
+        ],
         'require-jsdoc': [0]
     }
 };
