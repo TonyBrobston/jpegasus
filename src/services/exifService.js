3ce2f0e06c8da8a70d0be1a8bf65a6a8c0bda1e8
Tony Brobston <anthonybrobston@live.com>
2019-02-16 20:27:22 -0600

    Added two test for orientation 1 and 4 also refactored some magic markers

--- src/services/exifService.js
+++ src/services/exifService.js
@@ -1,35 +1,38 @@
-import exif from 'exif-js';
+const startOfFileMarker = 0xFFD8;
+const applicationSegmentOneMarker = 0xFFE1;
+const beginOfExifHeaderMarker = 0x45786966;
+const byteOrderMarker = 0x4949;
+const orientationMarker = 0x0112;
 
 const determineOrientation = async (file) => {
+
     return new Promise(resolve => {
         const reader = new FileReader();
 
         reader.onload = () => resolve((() => {
             const view = new DataView(reader.result);
+            if (view.getUint16(0, false) != startOfFileMarker) {
 
-            if (view.getUint16(0, false) != 0xFFD8) {
                 return;
             }
 
             const length = view.byteLength;
-
             let offset = 2;
 
             while (offset < length) {
                 const marker = view.getUint16(offset, false);
-
                 offset += 2;
 
-                if (marker == 0xFFE1) {
+                if (marker == applicationSegmentOneMarker) {
                     offset += 2;
 
-                    if (view.getUint32(offset, false) != 0x45786966) {
+                    if (view.getUint32(offset, false) != beginOfExifHeaderMarker) {
                         return;
                     }
 
                     offset += 6;
 
-                    const little = view.getUint16(offset, false) == 0x4949;
+                    const little = view.getUint16(offset, false) == byteOrderMarker;
 
                     offset += view.getUint32(offset + 4, little);
 
@@ -38,7 +41,7 @@ const determineOrientation = async (file) => {
                     offset += 2;
 
                     for (var i = 0; i < tags; i++) {
-                        if (view.getUint16(offset + (i * 12), little) == 0x0112) {
+                        if (view.getUint16(offset + (i * 12), little) == orientationMarker) {
                             return view.getUint16(offset + (i * 12) + 8, little);
                         }
                     }
