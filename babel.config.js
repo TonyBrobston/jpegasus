8263d62b43708897af4cd6865c42a4f0baa5a632
Tony Brobston <tbrobston@hy-vee.com>
2019-02-19 21:58:34 -0600

    Switching to babel env

--- babel.config.js
+++ babel.config.js
@@ -3,12 +3,17 @@ module.exports = function(api) {
     return {
         presets: [
             [
-                "@babel/preset-env", {
-                    "targets": {
-                        "node": "current"
-                    }
-                }
-            ]
+                "@babel/env",
+                {
+                    targets: {
+                        edge: "11",
+                        firefox: "60",
+                        chrome: "67",
+                        safari: "11.1",
+                    },
+                    useBuiltIns: "usage",
+                },
+            ],
         ]
     };
 };
