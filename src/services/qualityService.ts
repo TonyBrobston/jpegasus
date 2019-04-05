2beb7fc189d5cb7c2046c2cbaae9cadb4f0ad872
Tony Brobston <anthonybrobston@live.com>
2019-04-04 23:22:00 -0500

    Get qualityService tests passing

--- src/services/qualityService.ts
+++ src/services/qualityService.ts
@@ -2,16 +2,8 @@ import windowService from './windowService';
 import blobService from './blobService';
 import {Options} from "../types/Options";
 
-const determineQuality = (file: File, options: Options) => {
-    if (options.quality) {
-        return options.quality;
-    }
-
-    return 1.00;
-};
-
 const toFile = (file: File, canvas: any, options: Options) => {
-    const quality = determineQuality(file, options);
+    const quality = options.quality ? options.quality : 1.00;
     const dataUrl = canvas.toDataURL('image/jpeg', quality);
     const base64 = dataUrl.split(',')[1];
     const byteArray = windowService.toByteArray(base64);
