1c82d6715bd7a6b6345d3ed2ba6b2206696949bc
Tony Brobston <tbrobston@hy-vee.com>
2019-04-08 17:47:12 -0500

    Handle null and undefined scenarios better

--- src/services/canvasService.ts
+++ src/services/canvasService.ts
@@ -10,48 +10,48 @@ function setCanvasDimensions(canvas, orientation, scaledHeight, scaledWidth) {
     }
 }
 
-const correctExifRotation = (context: CanvasTransform | null, orientation: number, height: number, width: number) => {
-    //todo: test context null
-    if (context) {
-        switch (orientation) {
-        case 2:
-            context.transform(- 1, 0, 0, 1, width, 0);
-            break;
-        case 3:
-            context.transform(- 1, 0, 0, - 1, width, height);
-            break;
-        case 4:
-            context.transform(1, 0, 0, - 1, 0, height);
-            break;
-        case 5:
-            context.transform(0, 1, 1, 0, 0, 0);
-            break;
-        case 6:
-            context.transform(0, 1, - 1, 0, height, 0);
-            break;
-        case 7:
-            context.transform(0, - 1, - 1, 0, height, width);
-            break;
-        case 8:
-            context.transform(0, - 1, 1, 0, 0, width);
-            break;
-        default:
-            break;
-        }
+const correctExifRotation = (context: CanvasTransform, orientation: number, height: number, width: number) => {
+    switch (orientation) {
+    case 2:
+        context.transform(- 1, 0, 0, 1, width, 0);
+        break;
+    case 3:
+        context.transform(- 1, 0, 0, - 1, width, height);
+        break;
+    case 4:
+        context.transform(1, 0, 0, - 1, 0, height);
+        break;
+    case 5:
+        context.transform(0, 1, 1, 0, 0, 0);
+        break;
+    case 6:
+        context.transform(0, 1, - 1, 0, height, 0);
+        break;
+    case 7:
+        context.transform(0, - 1, - 1, 0, height, width);
+        break;
+    case 8:
+        context.transform(0, - 1, 1, 0, 0, width);
+        break;
+    default:
+        break;
     }
 };
 
 const create = async (file: File, image: HTMLImageElement, scale: number) => {
     const canvas = document.createElement('canvas');
-    const scaledHeight = image.height * scale;
-    const scaledWidth = image.width * scale;
-    const orientation = await exifService.determineOrientation(file);
-    setCanvasDimensions(canvas, orientation, scaledHeight, scaledWidth);
     const context = canvas.getContext('2d');
-    correctExifRotation(context, orientation, scaledHeight, scaledWidth);
-    //todo: test context null
-    context && context.drawImage(image, 0, 0, scaledWidth, scaledHeight);
-    return canvas;
+    if (context) {
+        const scaledHeight = image.height * scale;
+        const scaledWidth = image.width * scale;
+        const orientation = await exifService.determineOrientation(file);
+        setCanvasDimensions(canvas, orientation, scaledHeight, scaledWidth);
+        correctExifRotation(context, orientation, scaledHeight, scaledWidth);
+        context && context.drawImage(image, 0, 0, scaledWidth, scaledHeight);
+        return canvas;
+    } else {
+        throw 'Could not get CanvasRenderingContext2D from HTMLCanvasElement.'
+    }
 };
 
 export default {
