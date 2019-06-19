8bdb95f8cb71249171cac6935e2b97cff89ef090
Tony Brobston <tbrobston@hy-vee.com>
2019-06-19 09:56:51 -0500

    Fixing linting

--- src/services/imageService.ts
+++ src/services/imageService.ts
@@ -1,7 +1,7 @@
 import {Options} from '../types/Options';
 
 const create = async (file: File, options: Options): Promise<HTMLImageElement> => {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve: (image: HTMLImageElement) => void, reject: (error: ErrorEvent) => void): void => {
         const image = new Image();
         if (options.allowCrossOriginResourceSharing) {
             image.crossOrigin = 'Anonymous';
