a583082d32364cf12b46291ba0db536a83e9eafc
Tony Brobston <anthonybrobston@live.com>
2019-10-10 18:50:03 -0500

    Added cypress-file-upload and implemented a high level ui test

--- cypress/integration/ui.spec.js
+++ cypress/integration/ui.spec.js
@@ -6,6 +6,21 @@ context('UI', () => {
   });
 
   it('first test', () => {
-    cy.get('#imageInput').click();
+    const fileName = 'merlin-superJumo.jpg';
+    cy.fixture(fileName).then((fileContent) => {
+      cy.get('#imageInput').upload({
+        fileContent,
+        fileName,
+        mimeType: 'image/jpeg'
+      });
+      cy.get('#qualitySelector').select('0.5');
+
+      cy.get('input[value="Submit"]').click();
+
+      cy.get('#originalSize').should('have.text', '533.32');
+      cy.get('#originType').should('have.text', 'image/jpeg');
+      cy.get('#compressedSize').should('have.text', '50.29');
+      cy.get('#compressedType').should('have.text', 'image/jpeg');
+    });
   });
 });
