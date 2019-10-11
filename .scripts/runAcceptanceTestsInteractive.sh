c78a7dc2fc2f286f132d3f756e9361da8afda5c8
Tony Brobston <anthonybrobston@live.com>
2019-10-10 20:37:49 -0500

    Adding returns to shell script

--- .scripts/runAcceptanceTestsInteractive.sh
+++ .scripts/runAcceptanceTestsInteractive.sh
@@ -8,7 +8,7 @@ yarn link jpegasus
 yarn
 yarn start & wait-on http://localhost:5000
 cd ../
-yarn run cypress open
-  && rm -rf jpegasus-demo
-  && kill $(lsof -t -i:5000) & wait
+yarn run cypress open \
+  && rm -rf jpegasus-demo \
+  && kill $(lsof -t -i:5000) & wait \
   && yarn unlink
